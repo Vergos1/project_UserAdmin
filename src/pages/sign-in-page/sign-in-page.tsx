f727f729bb8d70a0215058856e1acf36188824f5
KILL0real <divannijexpert@gmail.com>
2025-01-03 16:14:38 +0200

    TSK-4: [feat] added background image for sign-in page

--- src/pages/sign-in-page/sign-in-page.tsx
+++ src/pages/sign-in-page/sign-in-page.tsx
@@ -14,11 +14,12 @@ import {
   Flex,
   Anchor,
   Title,
-  Badge
+  BackgroundImage
 } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { IconX, IconCheck } from '@tabler/icons-react'
 import StripeIcon from '../../assets/icons/Stripelogo.svg'
+import Background from '../../assets/images/app-background.jpg'
 
 function PasswordRequirement({ meets, label }: { meets: boolean; label: string }) {
   return (
@@ -75,114 +76,121 @@ const SignInPage: React.FC = () => {
   const color = strength === 100 ? 'teal' : strength > 50 ? 'yellow' : 'red'
 
   return (
-    <Center style={{ height: '100vh' }}>
-      <Flex direction='column' gap={30}>
-        <Box pl={0}>
-          <img src={StripeIcon} alt='Stripe Logo' width={58} height={24} />
-        </Box>
-        <Box
-          style={{
-            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
-            borderRadius: '8px',
+    <BackgroundImage src={Background}>
+      <Center style={{ height: '100vh' }}>
+        <Flex direction='column' gap={30}>
+          <Box pl={0}>
+            <img src={StripeIcon} alt='Stripe Logo' width={58} height={24} />
+          </Box>
+          <Box
+            style={{
+              boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
+              borderRadius: '8px',
+              backgroundColor: '#FFFFFF',
+              padding: '50px'
+            }}
+          >
+            <form onSubmit={form.onSubmit(values => console.log(values))}>
+              <Title order={3} pb={25} c='#505050'>
+                Sign in to your account
+              </Title>
+              <Flex direction='column' gap={20}>
+                <Box w={406}>
+                  <TextInput
+                    size='md'
+                    c='#494949'
+                    label='Email'
+                    placeholder='your@email.com'
+                    {...form.getInputProps('email')}
+                    error={form.errors.email}
+                    labelProps={{ style: { marginBottom: '10px' } }}
+                  />
+                </Box>
+                <Popover
+                  opened={popoverOpened}
+                  position='bottom'
+                  width='target'
+                  transitionProps={{ transition: 'pop' }}
+                >
+                  <Popover.Target>
+                    <div onFocusCapture={() => setPopoverOpened(true)} onBlurCapture={() => setPopoverOpened(false)}>
+                      <Flex justify='space-between' align='center' mb='xs'>
+                        <Text size='sm' fw={500} c='#494949'>
+                          Password
+                        </Text>
+                        <Anchor href='/forgot-password' size='sm' td='none'>
+                          Forgot your password?
+                        </Anchor>
+                      </Flex>
+                      <PasswordInput
+                        size='md'
+                        placeholder='Your password'
+                        value={password}
+                        onChange={event => {
+                          setPassword(event.currentTarget.value)
+                          form.setFieldValue('password', event.currentTarget.value)
+                        }}
+                        error={form.errors.password}
+                      />
+                    </div>
+                  </Popover.Target>
+                  <Popover.Dropdown>
+                    <Progress color={color} value={strength} size={5} mb='xs' />
+                    <PasswordRequirement label='Includes at least 6 characters' meets={password.length > 5} />
+                    {checks}
+                  </Popover.Dropdown>
+                </Popover>
 
-            padding: '50px'
-          }}
-        >
-          <form onSubmit={form.onSubmit(values => console.log(values))}>
-            <Title order={3} pb={25} c='#505050'>
-              Sign in to your account
-            </Title>
-            <Flex direction='column' gap={20}>
-              <Box w={406}>
-                <TextInput
-                  size='md'
+                <Checkbox
                   c='#494949'
-                  label='Email'
-                  placeholder='your@email.com'
-                  {...form.getInputProps('email')}
-                  error={form.errors.email}
-                  labelProps={{ style: { marginBottom: '10px' } }}
+                  mt='md'
+                  label='Stay signed in for a week'
+                  {...form.getInputProps('termsOfService', { type: 'checkbox' })}
                 />
-              </Box>
-              <Popover opened={popoverOpened} position='bottom' width='target' transitionProps={{ transition: 'pop' }}>
-                <Popover.Target>
-                  <div onFocusCapture={() => setPopoverOpened(true)} onBlurCapture={() => setPopoverOpened(false)}>
-                    <Flex justify='space-between' align='center' mb='xs'>
-                      <Text size='sm' fw={500} c='#494949'>
-                        Password
-                      </Text>
-                      <Anchor href='/forgot-password' size='sm' td='none'>
-                        Forgot your password?
-                      </Anchor>
-                    </Flex>
-                    <PasswordInput
-                      size='md'
-                      placeholder='Your password'
-                      value={password}
-                      onChange={event => {
-                        setPassword(event.currentTarget.value)
-                        form.setFieldValue('password', event.currentTarget.value)
-                      }}
-                      error={form.errors.password}
-                    />
-                  </div>
-                </Popover.Target>
-                <Popover.Dropdown>
-                  <Progress color={color} value={strength} size={5} mb='xs' />
-                  <PasswordRequirement label='Includes at least 6 characters' meets={password.length > 5} />
-                  {checks}
-                </Popover.Dropdown>
-              </Popover>
 
-              <Checkbox
-                c='#494949'
-                mt='md'
-                label='Stay signed in for a week'
-                {...form.getInputProps('termsOfService', { type: 'checkbox' })}
-              />
+                <Button w='100%' type='submit' mt='md' size='md'>
+                  Continue
+                </Button>
 
-              <Button w='100%' type='submit' mt='md' size='md'>
-                Continue
-              </Button>
-
-              <Anchor ta='center' href='/sign-on' size='sm' td='none'>
-                Use single sign-on (SSO) instead
+                <Anchor ta='center' href='/sign-on' size='sm' td='none'>
+                  Use single sign-on (SSO) instead
+                </Anchor>
+              </Flex>
+            </form>
+          </Box>
+          <Flex direction='column' gap={20}>
+            <Flex gap={3} pl={20}>
+              <Text size='sm' c='#494949'>
+                Don’t have an account?
+              </Text>
+              <Anchor size='sm' href='/sign-in'>
+                Sign up
               </Anchor>
             </Flex>
-          </form>
-        </Box>
-        <Flex direction='column' gap={20}>
-          <Flex gap={3} pl={20}>
-            <Text size='sm' c='#494949'>
-              Don’t have an account?
-            </Text>
-            <Anchor size='sm' href='/sign-in'>
-              Sign up
-            </Anchor>
-          </Flex>
-          <Flex gap={5} pl={20}>
-            <Text
-              size='sm'
-              style={{
-                color: '#494949',
-                textTransform: 'capitalize',
+            <Flex gap={5} pl={20}>
+              <Text
+                size='sm'
+                style={{
+                  color: '#494949',
+                  textTransform: 'capitalize',
 
-                fontWeight: 400,
-                cursor: 'pointer'
-              }}
-            >
-              © Stripe
-            </Text>
-            <Anchor td='none' c='#494949' size='sm'>
-              • Contact •
-            </Anchor>
-            <Anchor td='none' c='#494949' size='sm'>
-              Privacy & terms
-            </Anchor>
+                  fontWeight: 400,
+                  cursor: 'pointer'
+                }}
+              >
+                © Stripe
+              </Text>
+              <Anchor td='none' c='#494949' size='sm'>
+                • Contact •
+              </Anchor>
+              <Anchor td='none' c='#494949' size='sm'>
+                Privacy & terms
+              </Anchor>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Center>
+      </Center>
+    </BackgroundImage>
   )
 }
 
