a035a781523fd6930f37551c93ccd1f6bad04081
Yaroslav <andrexvich@icloud.com>
2025-01-05 00:39:55 +0200

    TSK-10 [feant] create NavBar in home-page.tsx Create StoreNameDropdown.tsx component and add in home-page.tsx. Create CustomNavLink.scss and add in home-page.tsx

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,7 +1,32 @@
-import { Loader } from '@mantine/core'
+import { AppShell, Burger, Group } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { MantineLogo } from '@mantinex/mantine-logo'
 import React from 'react'
+import StoreNameDropdown from './component/StoreNameDropdown'
 
 const HomePage: React.FC = () => {
-  return <>home</>
+  const [opened, { toggle }] = useDisclosure()
+  return (
+    <AppShell
+      layout='alt'
+      header={{ height: '10%' }}
+      navbar={{ width: '20%', breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding='md'
+    >
+      <AppShell.Header>
+        <Group h='100%' px='md'>
+          <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+          <MantineLogo size={30} />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p='md'>
+        <Group>
+          <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+        </Group>
+        <StoreNameDropdown/>
+      </AppShell.Navbar>
+      <AppShell.Main>Alt layout – Navbar and Aside are rendered on top on Header and Footer</AppShell.Main>
+    </AppShell>
+  )
 }
 export default HomePage
