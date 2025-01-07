9dd99d20145758af05e95dc95c9d0038c143df88
KILL0real <divannijexpert@gmail.com>
2025-01-07 12:35:11 +0200

    TSK-15:[fix] fix naming of folders and files in the app

--- src/components/app-navbar/store-name-btn/store-name-btn.tsx
+++ src/components/app-navbar/store-name-btn/store-name-btn.tsx
@@ -1,12 +1,10 @@
 import { NavLink } from '@mantine/core'
-import "./store-name.scss"
-import StoreLogo from './../../../assets/icons/navBar/StoreName.svg';
-import payments from './../../../assets/icons/navBar/sexshop.svg';
-
-
+import s from './store-name.module.scss'
+import StoreLogo from './../../../assets/icons/navBar/StoreName.svg'
+import payments from './../../../assets/icons/navBar/sexshop.svg'
 
 function StoreNameBtn() {
-  const styleLogo = { width: '1rem', height: '1rem' };
+  const styleLogo = { width: '1rem', height: '1rem' }
 
   return (
     <>
@@ -14,12 +12,15 @@ function StoreNameBtn() {
         className='navlink-store'
         label='Store name'
         leftSection={<img src={StoreLogo} alt='store' style={styleLogo} />}
-
       >
-        <NavLink label='Sex Shop' className='navlink-home' leftSection={<img src={payments} alt='Home' style={styleLogo} />} />
-        <NavLink label='Sex Shop' leftSection={<img src={payments} alt='Payments' style={styleLogo} />}/>
-        <NavLink label='Sex Shop' leftSection={<img src={payments} alt='Balances' style={styleLogo} />}/>
-        <NavLink label='Sex Shop' leftSection={<img src={payments} alt='Customers' style={styleLogo} />}/>
+        <NavLink
+          label='Sex Shop'
+          className='navlink-home'
+          leftSection={<img src={payments} alt='Home' style={styleLogo} />}
+        />
+        <NavLink label='Sex Shop' leftSection={<img src={payments} alt='Payments' style={styleLogo} />} />
+        <NavLink label='Sex Shop' leftSection={<img src={payments} alt='Balances' style={styleLogo} />} />
+        <NavLink label='Sex Shop' leftSection={<img src={payments} alt='Customers' style={styleLogo} />} />
       </NavLink>
     </>
   )
